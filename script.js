474f6024072133ba60399e975b2e9081944f9112
Egon Dohr <egondohr03@gmail.com>
2022-07-07 12:16:00 +0300

    grid function

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ function setGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for(let i = 0; i < size*size; i++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.textContent = 'D';
-        gridDiv.classList.add('grid-element')
-        grid.appendChild(gridDiv);
+    for(let i = 0; i < size; i++) {
+        for(let j = 0; j < size; j++) {
+            const gridDiv = document.createElement('div');
+            // gridDiv.textContent = 'D';
+            gridDiv.classList.add('grid-element');
+            if(j == 0) {
+                gridDiv.classList.add('lborder');    
+            }
+            if(i == 0) {
+                gridDiv.classList.add('uborder');
+            }
+            grid.appendChild(gridDiv);
+            
+            
+        }
     }
 }
 
