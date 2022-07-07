634d2db2b9be86dd0ada11ccf61d4b9aa319165e
Egon Dohr <egondohr03@gmail.com>
2022-07-07 13:47:19 +0300

    can draw

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector("#page div")
-
+let ok = 0
 function setGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -15,11 +15,24 @@ function setGrid(size) {
             if(i == 0) {
                 gridDiv.classList.add('uborder');
             }
+            gridDiv.addEventListener('mousedown', changeColor)
+            document.addEventListener('mouseup', () => ok = 0)
+            gridDiv.addEventListener('mouseover', changeColor)
             grid.appendChild(gridDiv);
             
             
         }
     }
+}
+
+function changeColor(e) { 
+    
+    if(e.type == 'mousedown') ok = 1
+    if(e.type == 'mouseover' && !ok) return
+    const R = Math.floor(Math.random() * 256)
+    const G = Math.floor(Math.random() * 256)
+    const B = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
 }
 
 setGrid(16);
